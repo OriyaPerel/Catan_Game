dbf40bdde1127f961e6ee0a372cd6074bdf719ef
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-07 11:50:44 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,49 +1,90 @@
 #include "Player.hpp"
 #include "Board.hpp"
 using namespace std;
-void Player::printRoads()const
+
+// Player::~Player(){
+//     for (Vertice *v : placesOfPlayer)
+//     {
+//         delete v;
+//     }
+//     for (Edge *e : roads)
+//     {
+//         delete e;
+//     }
+
+// }
+
+int Player::getAmountOfResource(ResourceType resource) const
 {
-    cout << "Player " << name << " has the following roads: ;" << endl;
+    if (resources.find(resource) != resources.end())
+    {
+        return resources.at(resource);
+    }
+    else
+    {
+
+        return 0;
+    }
+}
+
+void Player::printRoads() const
+{
+    cout << name << " has the following roads: " << endl;
     for (Edge *e : roads)
     {
         cout << e->toString() << endl;
     }
 }
-std::ostream& operator<<(std::ostream& os, const ResourceType& resourceType) {
-    switch (resourceType) {
-        case ResourceType::wool:
-            os << "Wood";
-            break;
-        case ResourceType::brick:
-            os << "Brick";
-            break;
-        case ResourceType::lumber:
-            os << "Lumber";
-            break;
-        case ResourceType::grain:
-            os << "grain";
-            break;
-        case ResourceType::ore:
-            os << "Ore";
-            break;
-        default:
-            os << "Unknown";
-            break;
+void Player::printPlaces() const
+{
+    cout << name << " has the following settlements: " << endl;
+    for (Vertice *v : placesOfPlayer)
+    {
+        cout << v->toString() << endl;
     }
-    return os;
 }
+
 void Player::printResources() const
 {
-    cout << "Player " << name << " has the following resources: " << endl;
     for (const auto &resource : resources)
     {
         cout << "Resource: " << resource.first << ", Amount: " << resource.second << endl;
     }
 }
 
+void Player::printPoints() const
+{
+    cout << "Player " << name << " has " << points << " points" << endl;
+}
+
 void Player::RemoveOraddResource(ResourceType resource, int count)
 {
     resources[resource] += count;
+}
+
+int Player::getAmountOfResources() const
+{
+    int sum = 0;
+    for (const auto &resource : resources)
+    {
+        sum += resource.second;
+    }
+    return sum;
+}
+
+void Player::removeMaxResource()
+{
+    int max = 0;
+    ResourceType maxResource;
+    for (const auto &resource : resources)
+    {
+        if (resource.second > max)
+        {
+            max = resource.second;
+            maxResource = resource.first;
+        }
+    }
+    RemoveOraddResource(maxResource, -1);
 }
 
 Vertice *findCommonVertexInBoard(const Hexigon &h1, const Hexigon &h2, const Hexigon &h3)
@@ -93,12 +134,9 @@ bool hasNeighbour(Vertice *v, std::vector<Edge *> h1e, std::vector<Edge *> h2e, 
     {
         if (e->thisVerticeInEdge(v) && e->getOtherVertice(v)->getisSettlement() == true)
         {
-            //  std::cout<<e->toString()<<std::endl;
             return true;
         }
     }
-    // std::cout<<"no neighbour"<<std::endl;
-
     return false;
 }
 
@@ -123,34 +161,22 @@ bool Player::Has2RoadsBetween(Vertice *v)
 
 void Player::placeSettelemnt(std::vector<std::string> places, std::vector<int> placesNum, Board &board)
 {
-    if (placesOfPlayer.size() > 2 && (resources[ResourceType::brick] < 1 || resources[ResourceType::lumber] < 1 || resources[ResourceType::wool] < 1 || resources[ResourceType::grain] < 1))
+    if (hisTurn == false)
     {
-        throw std::invalid_argument("player can't by a settlement");
+        throw std::invalid_argument("It's not your turn");
+    }
+    if (placesOfPlayer.size() >= 2 && (this->resources[ResourceType::brick] < 1 ||this-> resources[ResourceType::lumber] < 1 || this->resources[ResourceType::wool] < 1 || resources[ResourceType::grain] < 1))
+    {
+        throw std::invalid_argument("player can't buy a settlement");
     }
     Hexigon *hex1 = board.getHexigon(places[0], placesNum[0]);
-    // vector<Vertice *> h1 = hex1->getVertices();
-    //  std::cout<<"the edes of hex1:"<<hex1->getType()<<std::endl;
-    //  hex1->printEdges();
-    //  std::cout << "h1 number: " << hex1->getNumber() << std::endl;
-    //  std::cout << "h1 type: " << hex1->getType() << std::endl;
-    //  std ::cout << "h1 vertices: " << hex1->getVertices()[1]->getid() << std::endl;
-    //  hex1->printVertices();
     vector<Edge *> h1e = hex1->getEdges();
     Hexigon *hex2 = board.getHexigon(places[1], placesNum[1]);
-    // std::cout<<"the edes of hex2:"<<hex2->getType()<<std::endl;
-    // hex2->printEdges();
-    // hex2->printVertices();
-    // vector<Vertice *> h2 = hex2->getVertices();
     vector<Edge *> h2e = hex2->getEdges();
     Hexigon *hex3 = board.getHexigon(places[2], placesNum[2]);
-    // vector<Vertice *> h3 = hex3->getVertices();
     vector<Edge *> h3e = hex3->getEdges();
-    // std::cout<<"the edes of hex3:"<<hex3->getType()<<std::endl;
-    // hex3->printEdges();
-    //  hex3->printVertices();
-    // std::cout << "h3 number: " << hex3->getVertices()[2]->getid() << std::endl;
-    Vertice *v = findCommonVertexInBoard(*hex1, *hex2, *hex3); // the common vertex of the three hexigons.
-    std::cout << "v id: " << v->getid() << std::endl;
+    Vertice *v = findCommonVertexInBoard(*hex1, *hex2, *hex3); // the common vertex of the three hexigons
+
     if (v == nullptr) // there is no common vertex
     {
         throw std::invalid_argument("There is no common vertex in this places");
@@ -164,15 +190,13 @@ void Player::placeSettelemnt(std::vector<std::string> places, std::vector<int> p
     {
         throw std::invalid_argument("There is already a settlement in this edge");
     }
-    if (placesOfPlayer.size() > 2 && Has2RoadsBetween(v) == false) // there are no 2 roads between the vertex and the player's settlements
+    if (placesOfPlayer.size() >= 2 && Has2RoadsBetween(v) == false) // there are no 2 roads between the vertex and the player's settlements
     {
         throw std::invalid_argument("There are no 2 roads between the vertex and the player's settlements");
     }
     v->setOwner(this);
-    // std::cout << "v owner: " << v->getOwner()->getName() << std::endl;
     v->setPieceType(PieceType::Settlement);
     v->setisSettlement(true);
-    // std::cout << "v piece type: " << v->getPieceType() << std::endl;
 
     this->points += 1;
     if (this->placesOfPlayer.size() > 2)
@@ -191,16 +215,13 @@ void Player::placeSettelemnt(std::vector<std::string> places, std::vector<int> p
     }
     this->placesOfPlayer.push_back(v); // add this settlement to the player's settlements
 }
+
+
 Edge *findCommonEdgeInBoard(const Hexigon &h1, const Hexigon &h2)
 {
     // Get the edges of each hexagon
-    std::cout << "whattttttttttttttttttt" << std::endl;
     std::vector<Edge *> edges1 = h1.getEdges();
     std::vector<Edge *> edges2 = h2.getEdges();
-    //  std::cout<<"the edes of hex3:"<<h1.getType()<<std::endl;
-    // h1.printEdges();
-    // std::cout<<"the edes of hex3:"<<h2.getType()<<std::endl;
-    // h2.printEdges();
 
     // Iterate over the edges of the first hexagon
     for (Edge *e1 : edges1)
@@ -210,7 +231,6 @@ Edge *findCommonEdgeInBoard(const Hexigon &h1, const Hexigon &h2)
         {
             if (e1->getV1() == e2->getV1() && e1->getV2() == e2->getV2() || e1->getV1() == e2->getV2() && e1->getV2() == e2->getV1())
             {
-                std::cout << "the common edge isssssss:" << e1->toString() << std::endl;
                 return e1;
             }
         }
@@ -243,16 +263,16 @@ bool hasEdgeNeighbour(Edge *e, std::vector<Edge *> roads)
 
 void Player::placeRoad(std::vector<std::string> places, std::vector<int> placesNum, Board Board)
 {
-    if (roads.size() > 2 && (resources[ResourceType::brick] < 1 || resources[ResourceType::lumber] < 1))
+    if (hisTurn == false)
+    {
+        throw std::invalid_argument("It's not your turn");
+    }
+    if (roads.size() > 2 && (this->resources[ResourceType::brick] < 1 || this->resources[ResourceType::lumber] < 1))
     {
         throw std::invalid_argument("player can't buy a road");
     }
     Hexigon *hex1 = Board.getHexigon(places[0], placesNum[0]);
     Hexigon *hex2 = Board.getHexigon(places[1], placesNum[1]);
-   // std::cout << "the edes of hex3:" << hex1->getType() << std::endl;
-    //hex1->printEdges();
-    //std::cout << "the edes of hex3:" << hex2->getType() << std::endl;
-   // hex2->printEdges();
     Edge *e = findCommonEdgeInBoard(*hex1, *hex2);
     if (e == nullptr)
     {
@@ -268,7 +288,7 @@ void Player::placeRoad(std::vector<std::string> places, std::vector<int> placesN
         throw std::invalid_argument("There is no settlement in this edge");
     }
 
-    if (roads.size() > 2 && hasEdgeNeighbour(e, roads) == false)
+    if (roads.size() >=2 && hasEdgeNeighbour(e, roads) == false)
     {
         throw std::invalid_argument("There is not a path to this edge");
     }
@@ -280,4 +300,56 @@ void Player::placeRoad(std::vector<std::string> places, std::vector<int> placesN
         RemoveOraddResource(ResourceType::lumber, -1);
     }
     this->roads.push_back(e);
+}
+
+void Player::chengeSettlementToCity(std::vector<std::string> places, std::vector<int> placesNum, Board &board)
+{
+    if (this->hisTurn == false)
+    {
+        throw std::invalid_argument("It's not your turn");
+    }
+    if (this->resources[ResourceType::ore] < 3 || this->resources[ResourceType::grain] < 2)
+    {
+        throw std::invalid_argument("player can't buy a city");
+    }
+    Hexigon *hex1 = board.getHexigon(places[0], placesNum[0]);
+    Hexigon *hex2 = board.getHexigon(places[1], placesNum[1]);
+    Hexigon *hex3 = board.getHexigon(places[2], placesNum[2]);
+
+    Vertice *v = findCommonVertexInBoard(*hex1, *hex2, *hex3); // the common vertex of the three hexigons
+    if (v == nullptr) // there is no common vertex
+    {
+        throw std::invalid_argument("There is no common vertex in this places");
+    }
+
+     if (v->getOwner() != this) // there are already a settlement in this vertex
+    {
+        throw std::invalid_argument("You can't chenge this settlment, it's not your");
+    }
+
+    v->setPieceType(PieceType::City);
+    this->points += 1;
+    RemoveOraddResource(ResourceType::grain, -2);
+    RemoveOraddResource(ResourceType::ore, -3);
+}
+
+void Player::trade(Player *player, ResourceType give, ResourceType get, int amountget, int amountgive)
+{
+    if (hisTurn == false)
+    {
+        throw std::invalid_argument("It's not your turn");
+    }
+    if (this == player)
+    {
+        throw std::invalid_argument("You can't trade with yourself");
+    }
+
+    if (this->getAmountOfResource(give) < amountgive || player->getAmountOfResource(get) < amountget)
+    {
+        throw std::invalid_argument("You don't have enough resources to trade");
+    }
+    this->RemoveOraddResource(give, -amountgive);
+    this->RemoveOraddResource(get, amountget);
+    player->RemoveOraddResource(give, amountgive);
+    player->RemoveOraddResource(get, -amountget);
 }
