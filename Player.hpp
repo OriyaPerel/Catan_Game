404f2c33aafb15ff30a600ab2fdab5f21727368d
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-07 11:51:05 +0300

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -11,6 +11,7 @@
 // #include "Board.hpp"
 
 enum class ResourceType;
+enum class CardType;
 class Board;
 class Edge;
 class Vertice;
@@ -22,27 +23,37 @@ private:
     std::string name;
     std::vector<Vertice *> placesOfPlayer;
     std::vector<Edge *> roads;
-    //Vertice *findCommonVertexInBoard(std::vector<Vertice *> vertices1, std::vector<Vertice *> vertices2, std::vector<Vertice *> vertices3, std::vector<Vertice *> allVertices);
     std::map<ResourceType, int> resources;
     int points = 0;
+    int Knights = 0;
+    bool hisTurn = false;
 
 public:
+    Player() = default;
     Player(std::string name) : name(name){};
-    // void addVertice(Vertice *v) { this->places.push_back(v); };
+    ~Player();
+    bool isHisTurn() const { return hisTurn; }
+    void setHisTurn(bool hisTurn) { this->hisTurn = hisTurn; }
     bool Has2RoadsBetween(Vertice *v);
     void placeSettelemnt(std::vector<std::string> places, std::vector<int> placesNum, Board &board);
     void placeRoad(std::vector<std::string> places, std::vector<int> placesNum, Board Board);
-    void rollDice();
-    void endTurn();
-    void printPoints();
-    void trade(Player player, std::string give, std::string get, int amountget, int amountgive);
+    void printPoints() const;
+    void chengeSettlementToCity(std::vector<std::string> places, std::vector<int> placesNum, Board &board);
+    void trade(Player *player, ResourceType give, ResourceType get, int amountget, int amountgive);
     void RemoveOraddResource(ResourceType resource, int count);
-    std::string getName() { return name; }
-    int getPoints() { return points; }
-    std::vector<Vertice *> getPlacesOfPlayer() { return placesOfPlayer; } 
-    std::vector<Edge *> getRoads() { return roads; }
+    std::string getName() const { return name; }
+    int getPoints() const { return points; }
+    void upDatePoints(int points) { this->points += points; }
+    std::vector<Vertice *> getPlacesOfPlayer() const { return placesOfPlayer; }
+    std::vector<Edge *> getRoads() const { return roads; }
     void printRoads() const;
-    void printResources()const;
+    void printPlaces() const;
+    void printResources() const;
+    int getAmountOfResource(ResourceType resource) const;
+    int getKnights() const { return Knights; }
+    void addKnight() { Knights++; }
+    int getAmountOfResources() const;
+    void removeMaxResource();
 };
 
 #endif
