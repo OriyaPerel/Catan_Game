1f51bf83c63009d6a429aaa9fa355c76fd89d181
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-07 11:52:31 +0300

    Update catan.hpp

--- catan.hpp
+++ catan.hpp
@@ -1,16 +1,42 @@
 #include <vector>
 #include "Player.hpp"
 #include "Board.hpp"
+#include "developmentCard.hpp"
+#include <typeinfo>
+enum class CardType
+{
+    Knight,
+    Monopoly,
+    YearOfPlenty,
+    VictoryPoint,
+    RoadBuilding
+};
 
-
-class Catan {
-    private:
-    std::vector<Player> players;
+class Catan
+{
+private:
+    std::vector<Player *> players;
     Board board;
+    std::vector<developmentCard *> deck;
+   
 
-    public:
-    Catan(Player p1, Player p2, Player p3);
+public:
+    Catan(Player *p1, Player *p2, Player *p3);
+   // ~Catan();
     void ChooseStartingPlayer();
-    Board& getBoard();
-    void printWinner();
+    std::vector<Player *> getPlayers() { return players; }
+    Board &getBoard();
+    void printWinner()const;
+    void rollDice(Player *p);
+    void rollDice(Player *p, int num);
+    void endTurn(Player *p);
+    void removeHalfResourefromPleyer();
+    void initialDeck();
+    developmentCard *getCard(CardType card);
+    std::string cardTypeToString(CardType cardType);
+    void printPlayerInfo(Player* player);
+    std::string getCardType(developmentCard *card);
+
+    void getCardToPlayer(CardType cardType,Player* player);
+
 };
