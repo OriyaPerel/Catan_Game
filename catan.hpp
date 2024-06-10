37d7eb3d96d75ed22eb466b23fcadb5a41a68139
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-10 16:52:25 +0300

    Update catan.hpp

--- catan.hpp
+++ catan.hpp
@@ -3,40 +3,28 @@
 #include "Board.hpp"
 #include "developmentCard.hpp"
 #include <typeinfo>
-enum class CardType
-{
-    Knight,
-    Monopoly,
-    YearOfPlenty,
-    VictoryPoint,
-    RoadBuilding
-};
 
 class Catan
 {
 private:
-    std::vector<Player *> players;
-    Board board;
-    std::vector<developmentCard *> deck;
-   
+  std::vector<Player *> players;
+  Board board;
+  std::vector<developmentCard *> deck;
 
 public:
-    Catan(Player *p1, Player *p2, Player *p3);
-   // ~Catan();
-    void ChooseStartingPlayer();
-    std::vector<Player *> getPlayers() { return players; }
-    Board &getBoard();
-    void printWinner()const;
-    void rollDice(Player *p);
-    void rollDice(Player *p, int num);
-    void endTurn(Player *p);
-    void removeHalfResourefromPleyer();
-    void initialDeck();
-    developmentCard *getCard(CardType card);
-    std::string cardTypeToString(CardType cardType);
-    void printPlayerInfo(Player* player);
-    std::string getCardType(developmentCard *card);
-
-    void getCardToPlayer(CardType cardType,Player* player);
-
+  Catan(Player *p1, Player *p2, Player *p3);
+  ~Catan() ;
+  void ChooseStartingPlayer();
+  void initialDeck();
+  std::vector<Player *> getPlayers() const { return players; }
+  Board &getBoard();
+  std::string getCardType(developmentCard *card) const;
+  developmentCard *getCard2(CardType card, Player *p);
+  void printWinner() const;
+  void rollDice(Player *p);
+  void rollDice(Player *p, int num);
+  void endTurn(Player *p);
+  void removeHalfResourefromPleyer();
+  std::string cardTypeToString(CardType cardType) const;
+  void printPlayerInfo(Player *player) const;
 };
