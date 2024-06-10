07c7b0481ea45eb5662cc7b2ca72abedf96a2983
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-10 16:53:37 +0300

    Update developmentCard.cpp

--- developmentCard.cpp
+++ developmentCard.cpp
@@ -1,39 +1,27 @@
 #include "developmentCard.hpp"
 
-// class Catan;
-//  VictoryPoint ניצחון
-//  RoadBuilding בניית כביש
-//  YearOfPlenty שנת השפעה
-//  Monopoly מונופול
-//  Knight לוחם
-//  DevelopCard קלף פיתוח
 
 void developmentCard::startActive(Player *player)
 {
-    if (isUsed==true)
+    if (this->isBought == false)
     {
-        throw std::invalid_argument("The card is already used");
-    }
-    if (player->getAmountOfResource(ResourceType::ore) < 1 || player->getAmountOfResource(ResourceType::grain) < 1 || player->getAmountOfResource(ResourceType::wool) < 1)
-    {
-        throw std::invalid_argument("You don't have enough resources");
+        throw std::invalid_argument("The card is not Bought");
     }
     if (player->isHisTurn() == false)
     {
         throw std::invalid_argument("It's not your turn so you can't use the card");
     }
-
 }
 void developmentCard::endActive(Player *player)
 {
     player->RemoveOraddResource(ResourceType::ore, -1);
     player->RemoveOraddResource(ResourceType::grain, -1);
     player->RemoveOraddResource(ResourceType::wool, -1);
-    this->isUsed = true;
+    
 }
 
-developmentCard::~developmentCard() {
-    
+developmentCard::~developmentCard()
+{
 }
 Monopoly::Monopoly(std::vector<Player *> players)
 {
@@ -50,7 +38,6 @@ void Monopoly::active(Player *player)
 {
     if (resource == ResourceType::None)
     {
-        
         throw std::invalid_argument("You need to choose a resource");
     }
     startActive(player);
@@ -59,13 +46,13 @@ void Monopoly::active(Player *player)
     {
         if (p != player)
         {
-           
             player->RemoveOraddResource(resource, p->getAmountOfResource(resource));
             p->RemoveOraddResource(resource, -p->getAmountOfResource(resource));
         }
     }
-     std::cout<<"monopoly card is used"<<std::endl;
-    std::cout << "now player have: " << player->getAmountOfResource(ResourceType::brick) << std::endl;
+    player->addOrRemoveCard(CardType::Monopoly, -1);
+    std::cout << "monopoly card is used" << std::endl;
+
     endActive(player);
 }
 
@@ -75,6 +62,8 @@ void VictoryPoint::active(Player *player)
     startActive(player);
     player->upDatePoints(1);
     std::cout << "now player have: " << player->getPoints() << std::endl;
+    std::cout << "VictoryPoint card is used" << std::endl;
+    player->addOrRemoveCard(CardType::VictoryPoint, -1);
     endActive(player);
 }
 
@@ -84,7 +73,9 @@ void RoadBuilding::active(Player *player)
     startActive(player);
     player->RemoveOraddResource(ResourceType::brick, 1);
     player->RemoveOraddResource(ResourceType::lumber, 1);
-    std::cout << "now player have: " << player->getPoints() << std::endl;
+    player->addOrRemoveCard(CardType::RoadBuilding, -1);
+    std::cout << "RoadBuilding card is used" << std::endl;
+
     endActive(player);
 }
 void YearOfPlenty::setResources(ResourceType resource1, ResourceType resource2)
@@ -102,19 +93,21 @@ void YearOfPlenty::active(Player *player)
     startActive(player);
     player->RemoveOraddResource(resource1, 1);
     player->RemoveOraddResource(resource2, 1);
+    std::cout << "YearOfPlenty card is used" << std::endl;
+    player->addOrRemoveCard(CardType::YearOfPlenty, -1);
     endActive(player);
 }
 
 Knight::~Knight() {}
 void Knight::active(Player *player)
 {
-
     startActive(player);
-    player->addKnight();
-    std::cout << "now player have: " << player->getPoints() << std::endl;
-    if (player->getKnights() == 3)
+
+    if (player->getCardMap()[CardType::Knight] == 3)
     {
         player->upDatePoints(2);
     }
+    std::cout << "Knight card is used" << std::endl;
+
     endActive(player);
 }
