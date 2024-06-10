58c52110bd9ff70dc7e67b8612ce1b075f493f36
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-10 16:45:00 +0300

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,14 +1,11 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
-// #include "Vertice.hpp"
-// #include "Hexigon.hpp"
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <stdexcept>
 #include <map>
-// #include "Board.hpp"
 
 enum class ResourceType;
 enum class CardType;
@@ -24,36 +21,40 @@ private:
     std::vector<Vertice *> placesOfPlayer;
     std::vector<Edge *> roads;
     std::map<ResourceType, int> resources;
+    std::map<CardType, int> cards;
     int points = 0;
-    int Knights = 0;
     bool hisTurn = false;
 
 public:
     Player() = default;
     Player(std::string name) : name(name){};
-    ~Player();
-    bool isHisTurn() const { return hisTurn; }
-    void setHisTurn(bool hisTurn) { this->hisTurn = hisTurn; }
-    bool Has2RoadsBetween(Vertice *v);
-    void placeSettelemnt(std::vector<std::string> places, std::vector<int> placesNum, Board &board);
-    void placeRoad(std::vector<std::string> places, std::vector<int> placesNum, Board Board);
-    void printPoints() const;
-    void chengeSettlementToCity(std::vector<std::string> places, std::vector<int> placesNum, Board &board);
-    void trade(Player *player, ResourceType give, ResourceType get, int amountget, int amountgive);
-    void RemoveOraddResource(ResourceType resource, int count);
+    ~Player() {}
     std::string getName() const { return name; }
     int getPoints() const { return points; }
-    void upDatePoints(int points) { this->points += points; }
+    std::map<CardType, int> getCardMap() const { return cards; }
+    int getAmountOfResource(ResourceType resource) const;
+    int getAmountOfResources() const;
+    bool isHisTurn() const { return hisTurn; }
+    
     std::vector<Vertice *> getPlacesOfPlayer() const { return placesOfPlayer; }
     std::vector<Edge *> getRoads() const { return roads; }
+    
+    void printPoints() const;
     void printRoads() const;
     void printPlaces() const;
     void printResources() const;
-    int getAmountOfResource(ResourceType resource) const;
-    int getKnights() const { return Knights; }
-    void addKnight() { Knights++; }
-    int getAmountOfResources() const;
+
+    void setHisTurn(bool hisTurn) { this->hisTurn = hisTurn; }
+    bool Has2RoadsBetween(const Vertice *v) const;
+    void placeSettelemnt(std::vector<std::string> places, std::vector<int> placesNum, const Board &board);
+    void placeRoad(std::vector<std::string> places, std::vector<int> placesNum, const Board &Board);
+    void chengeSettlementToCity(std::vector<std::string> places, std::vector<int> placesNum, const Board &board);
+    void tradeResours(Player *player, ResourceType give, ResourceType get, int amountget, int amountgive);
+    void tradeCard(Player *player, CardType give, CardType get);
+    void RemoveOraddResource(ResourceType resource, int count);
+    void upDatePoints(int points) { this->points += points; }
     void removeMaxResource();
+    void addOrRemoveCard(CardType card, int count);
 };
 
 #endif
