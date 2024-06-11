c3030bb9f0977a3e64bfb99a6b0ed8f7f247990e
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-11 16:29:54 +0300

    Update developmentCard.hpp

--- developmentCard.hpp
+++ developmentCard.hpp
@@ -4,12 +4,12 @@ enum class ResourceType;
 #include <iostream>
 #include "Player.hpp"
 #include "Hexigon.hpp"
- // id:322522806
- // email:oriyaperel18@gmail.com
+// id:322522806
+// email:oriyaperel18@gmail.com
 
 enum class CardType
 {
-    Knight, 
+    Knight,
     Monopoly,
     YearOfPlenty,
     VictoryPoint,
@@ -23,10 +23,9 @@ public:
     virtual ~developmentCard();
     virtual void active(Player *player) = 0;
     void startActive(Player *player);
-    void endActive(Player *player);
     void setBought(bool isBought) { this->isBought = isBought; }
     bool getIsBought() const { return isBought; }
-    };
+};
 
 class Monopoly : public developmentCard
 {
@@ -38,19 +37,19 @@ public:
     Monopoly(std::vector<Player *> players);
     ~Monopoly();
     void active(Player *player) override;
-    void setResource(ResourceType resource) ;
+    void setResource(ResourceType resource);
 };
 
 class VictoryPoint : public developmentCard
 {
-    public:
+public:
     ~VictoryPoint();
     void active(Player *player) override;
 };
 
 class RoadBuilding : public developmentCard
 {
-    public:
+public:
     ~RoadBuilding();
     void active(Player *player) override;
 };
@@ -62,14 +61,14 @@ private:
     ResourceType resource2 = ResourceType::None;
 
 public:
-void setResources(ResourceType resource1,ResourceType resource2) ;
+    void setResources(ResourceType resource1, ResourceType resource2);
     ~YearOfPlenty();
     void active(Player *player) override;
 };
 
 class Knight : public developmentCard
 {
-    public:
+public:
     ~Knight();
     void active(Player *player) override;
 };
